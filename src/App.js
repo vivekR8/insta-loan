127936ca4dde8d80237a75ba91e4e5f4aeab508d
Vivek Raghuvanshi <you@example.com>
2021-09-01 13:26:13 +0530

    added axios get request for homeloan

--- src/App.js
+++ src/App.js
@@ -7,26 +7,36 @@ import {
 } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Sidebar from './components/SideBar';
-// import LoanDetails from './pages/LoanDetials';
+import LoanDetails from './pages/LoanDetials';
 import './App.css';
 
 class  App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      isLoggedIn:false
+    };
+  }
 
   render(){
   return (
     <div className="App">
-      <Router>
-          <div >
-              
-              <Sidebar />
-              {/* <LoanDetails value={"hi"} valueid={101}/> */}
-              <Route exact path="/" component={Dashboard}/>
-              <Route exact path="/loan-details" component={Dashboard}/>
-              <Route exact path="/apply-loan" component={Dashboard}/>
-              <Route exact path="/loan-status" component={Dashboard}/>
-              <Route exact path="/emi-calculator" component={Dashboard}/>
-          </div>
-      </Router>
+      {this.state.isLoggedIn?
+          <Router>
+              <div >
+                  
+                  {/* <Sidebar /> */}
+                  {/* <LoanDetails value={"hi"} valueid={101}/> */}
+                  <Route exact path="/" component={Dashboard}/>
+                  <Route exact path="/loan-details" ><LoanDetails valueId={100}/></Route>
+                  <Route exact path="/apply-loan" component={Dashboard}/>
+                  <Route exact path="/loan-status" component={Dashboard}/>
+                  <Route exact path="/emi-calculator" component={Dashboard}/>
+              </div>
+          </Router>
+        :
+        <button onClick={()=>{this.setState({isLoggedIn:true})}}>LOG IN</button>
+        }
     </div>
   );
   }
--- src/pages/LoanDetials.js
+++ src/pages/LoanDetials.js
@@ -1,11 +1,31 @@
-import React from ' react';
-
+import React from 'react';
+import axios from 'axios';
+import {API} from '../constants';
+import ShowData from '../components/showData';
 class LoanDetails extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            loanDetails:null
+        };
+    }
     
+    handleClick=()=>{
+        axios.get(`http://localhost:8090/home-loan`).then(
+            (res)=>{console.log(res.data)
+            this.setState({loanDetails:res.data})}
+        ).catch((err)=>console.log(err))
+    }
     render(){
         return(
             <div>
-                <div><h1>{this.props.valueid}</h1></div>
+                <div><h1 onClick={()=>this.handleClick()}>{this.props.valueId}</h1></div>
+                {/* {this.state.loanDetails.map((val)=>{ */}
+                {/* {this.state.loanDetails && */}
+                    <ShowData val={this.state.loanDetails}/>
+                {/* } */}
+                {/* })} */}
+                
             </div>
             
         );
